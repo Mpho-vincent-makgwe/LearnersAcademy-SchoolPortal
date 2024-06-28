be429d71059edc7e9b46ad314054c0246dbc5699
Mpho Vincent Makgwe <118048918+Mpho-vincent-makgwe@users.noreply.github.com>
2024-06-28 11:07:23 +0200

    Added controllers, models, DAO's and utils along side with their jsp files

---
+++ school-portal/src/main/java/com/learnersacademy/model/Admin.java
@@ -0,0 +1,7 @@
+package com.learnersacademy.model;
+
+public class Admin {
+	 private int id;
+	 private String username;
+	 private String password;
+}
---
+++ school-portal/src/main/java/com/learnersacademy/model/Class.java
@@ -0,0 +1,6 @@
+package com.learnersacademy.model;
+
+public class Class {
+	private int id;
+    private String name;
+}
